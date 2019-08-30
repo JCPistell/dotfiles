b338fd443e4749267fcabcd7d08c239feca2f100
Colin Pistell <colin.pistell@slalom.com>
2019-08-30 11:19:06 -0600

    adding neovim setup to script

--- setup.sh
+++ setup.sh
@@ -20,18 +20,34 @@ brew bundle
 
 # Set up symlinks
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+mkdir -p ~/.config/nvim
 
 ln -s $DIR/bashrc ~/.bashrc
 ln -s $DIR/tmux.conf ~/.tmux.conf
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/gitignore_global ~/.gitignore_global
-ln -s $Dir/spacemacs ~/.spacemacs
+ln -s $DIR/spacemacs ~/.spacemacs
+ln -s $DIR/flake8 ~/.config/flake8
+ln -s $DIR/init.vim ~/.config/nvim/init.vim
+
+# Set up virtualenvs
+
+pyenv install 3.7.4
+pyenv install 2.7.14
+pyenv global 3.7.4
+pyenv virtualenv 2.7.14 neovim2
+pyenv virtualenv 3.7.4 neovim3
+pyenv activate neovim2
+pip install neovim
+pyenv activate neovim3
+pip install neovim
+pip install flake8
+pip install jedi
+pyenv deactivate
+
 
 # Set up vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# install base python packages
-pip install virtualenv
-pip install virtualenvwrapper
-pip install flake8
+vim +PlugInstall +qall
